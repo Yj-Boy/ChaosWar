5560659376ca13f7f984c988d1759fcd1e2875f4
Yj-Boy <1529008728@qq.com>
2019-11-03 03:35:56 +0800

    封装了CreateThousandsOfTroops生成实体的方法，尝试用Invoke方法来生成“千军万马”，效果不理想

--- Assets/Scripts/ECS/CreatThousandsOfTroops.cs
+++ Assets/Scripts/ECS/CreatThousandsOfTroops.cs
@@ -7,28 +7,34 @@ using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
 using Unity.Rendering;
-
+using System;
 
 public class CreatThousandsOfTroops : MonoBehaviour
 {
     [SerializeField]
-    private int amount;     //实体总数
+    private int amount;                     //实体总数
     [SerializeField]
-    private float3 position;    //生成实体的位置
+    private float3 position;                //生成实体的位置
     [SerializeField]
-    private float moveSpeed;    //移动速度
+    private float moveSpeed;                //移动速度
     [SerializeField]
-    private float floatingSpeed,floatingTopBound,floatingBottomBound;   //浮动参数
+    private float floatingSpeed;            //浮动参数
     [SerializeField]
-    private float xNum;     //x轴的实体数量
+    private float floatingTopBound;         //浮动参数
     [SerializeField]
-    private Vector3 forwardVector;      //移动朝向（xyz）
+    private float floatingBottomBound;      //浮动参数
     [SerializeField]
-    private Mesh _mesh;     //实体渲染网格
+    private int xNum;                       //x轴的实体数量
+    //private int yNum;                       //生成多少列的实体
     [SerializeField]
-    private Material _material;     //实体渲染材质
+    private Vector3 forwardVector;          //移动朝向（xyz）                                    
+    [SerializeField]
+    private Mesh _mesh;                     //实体渲染网格
+    [SerializeField]
+    private Material _material;             //实体渲染材质
 
-    EntityManager entityManager;       //实体管理对象，用于创建实体原型（Archetype）和实体（Entity）
+    EntityManager entityManager;            //实体管理对象，用于创建实体原型（Archetype）和实体（Entity）
+    EntityArchetype entityArchetype;        //实体原型对象
 
     private void Start()
     {
@@ -36,7 +42,7 @@ public class CreatThousandsOfTroops : MonoBehaviour
         entityManager = World.Active.EntityManager;
 
         //创建并初始化实体原型
-        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+        entityArchetype = entityManager.CreateArchetype(
             typeof(MoveSpeedComponent),
             typeof(FloatingComponent),
             typeof(Translation),
@@ -46,8 +52,20 @@ public class CreatThousandsOfTroops : MonoBehaviour
             typeof(LocalToWorld)
             );
 
+        ////行数大于0，执行InvokeRepeating函数重复调用SpawnEntity生成实体
+        //yNum = amount / xNum;
+        ////Debug.Log("yNum:"+yNum);
+        //if(yNum>0)
+        //{
+        //    InvokeRepeating("SpawnEntity", 0.3f, 0.3f);
+        //}
+        SpawnEntity();
+    }
+
+    private void SpawnEntity()
+    {
         //创建实体数组
-        NativeArray<Entity> entityArr = new NativeArray<Entity>(amount,Allocator.Persistent);
+        NativeArray<Entity> entityArr = new NativeArray<Entity>(amount, Allocator.Persistent);
 
         //将实体原型和实体数组结合
         entityManager.CreateEntity(entityArchetype, entityArr);
@@ -56,13 +74,13 @@ public class CreatThousandsOfTroops : MonoBehaviour
         float positionX = 0f;
         float positionZ = 0f;
         //初始化每个实体对象
-        for(int i=0;i<entityArr.Length;i++)
+        for (int i = 0; i < entityArr.Length; i++)
         {
             entityManager.SetComponentData(entityArr[i], new MoveSpeedComponent { value = moveSpeed });
             float3 pos = new float3(
-                position.x+positionX - xNum / 2, 
-                position.y+noise.cnoise(new float2(positionX, 1f) * UnityEngine.Random.Range(0f, 1f)),
-                position.z+positionZ);
+                position.x + positionX - xNum / 2,
+                position.y + noise.cnoise(new float2(positionX, 1f) * UnityEngine.Random.Range(0f, 1f)),
+                position.z + positionZ);
             entityManager.SetComponentData(entityArr[i], new Translation
             {
                 Value = transform.TransformPoint(pos)
@@ -73,23 +91,32 @@ public class CreatThousandsOfTroops : MonoBehaviour
                 topBound = floatingTopBound,
                 bottomBound = floatingBottomBound,
                 floatingStartPosY = position.y
-            });    
+            });
             entityManager.SetComponentData(entityArr[i], new Rotation
             {
                 Value = Quaternion.Euler(forwardVector)
             });
             entityManager.SetSharedComponentData(entityArr[i], new RenderMesh
             {
-                mesh = _mesh, material = _material
+                mesh = _mesh,
+                material = _material
             });
             positionX++;
             if (positionX % xNum == 0)
             {
                 positionX = 0f;
                 positionZ -= 1f;
-            }             
+            }
         }
         //释放实体数组
         entityArr.Dispose();
+
+        ////每生产一行实体，行数减一
+        ////当行数小于0时，取消执行该脚本下的所有Invoke函数
+        //yNum--;
+        //if(yNum<=0)
+        //{
+        //    CancelInvoke();
+        //}
     }
 }
